a82ac29ee61c37cb6df7e483d78d5f1af6e8729a
marialbst <marialbst@gmail.com>
2017-08-03 19:44:48 +0300

    I-st part of lab added

---
+++ UnitTesting-Lab/Skeleton.Tests/AxeTests.cs
@@ -0,0 +1,45 @@
+﻿namespace Skeleton.Tests
+{
+    using System;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class AxeTests
+    {
+        private const int AxeAttack = 1;
+        private const int AxeDurability = 1;
+        private const int DummyHealth = 10;
+        private const int DummyExp = 10;
+
+        private Axe axe;
+        private Dummy dummy;
+
+        [SetUp]
+        public void TestInit()
+        {
+            //Arrange
+            this.axe = new Axe(AxeAttack, AxeDurability);
+            this.dummy = new Dummy(DummyHealth,DummyExp);
+        }
+        [Test]
+        public void AxeLosesDurabilityAfterAttack()
+        {
+            //Act
+            this.axe.Attack(this.dummy);
+
+            //Assert
+            Assert.AreEqual(0, this.axe.DurabilityPoints, "Axe Durability doesn't change after attack");
+        }
+
+        [Test]
+        public void AttackWithBrokenWeapon()
+        {
+            //Act
+            this.axe.Attack(this.dummy);
+
+            //Assert
+            var ex = Assert.Throws<InvalidOperationException>(() => this.axe.Attack(this.dummy));
+            Assert.AreEqual("Axe is broken.", ex.Message, "Messages are not the same.");
+        }
+    }
+}
---
+++ UnitTesting-Lab/Skeleton.Tests/DummyTests.cs
@@ -0,0 +1,71 @@
+﻿namespace Skeleton.Tests
+{
+    using System;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class DummyTests
+    {
+        private const int AliveDummyHealth = 10;
+        private const int MustDieDummyHealth = 4;
+        private const int DeadDummyHealth = 0;
+        private const int DummyExp = 10;
+        private const int AttackPower = 5;
+        
+        private Dummy dummy;
+
+        [SetUp]
+        public void TestInit()
+        {
+            //Arrange
+            this.dummy = new Dummy(AliveDummyHealth, DummyExp);
+        }
+
+        [Test]
+        public void DummyLosesHealthAfterAttack()
+        {
+            //Act
+            this.dummy.TakeAttack(AttackPower);
+
+            //Assert
+            Assert.AreEqual(AliveDummyHealth-AttackPower, this.dummy.Health, "Dummy doesn't lose health after attack.");
+        }
+
+        [Test]
+        public void DeadDummyThrowsExceptionAfterAttack()
+        {
+            //Arrange
+            this.dummy = new Dummy(MustDieDummyHealth, DummyExp);
+
+            //Act
+            this.dummy.TakeAttack(AttackPower);
+
+            //Assert
+            InvalidOperationException ex = Assert.Throws<InvalidOperationException>(() => this.dummy.TakeAttack(AttackPower));
+            Assert.AreEqual("Dummy is dead.", ex.Message, "Outputs are not the same.");
+        }
+
+        [Test]
+        public void DeadDummyGivesExp()
+        {
+            //Arrange
+            this.dummy = new Dummy(DeadDummyHealth, DummyExp);
+
+            //Act
+            this.dummy.IsDead();
+
+            //Assert
+            Assert.AreEqual(DummyExp, this.dummy.GiveExperience(), "Dummy does not give XP after his dead.");
+        }
+
+        [Test]
+        public void AliveDummyNotGiveExp()
+        {
+            //Act
+            this.dummy.IsDead();
+
+            //Assert
+            Assert.Throws<InvalidOperationException>(() => this.dummy.GiveExperience(), "Dummy gives XP even if he's alive.");
+        }
+    }
+}
